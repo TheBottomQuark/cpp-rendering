218125b8e3bc0beccb3972a83b5fdb3cb47ecc73
TheBottomQuark <119063498+TheBottomQuark@users.noreply.github.com>
2023-02-07 22:39:12 +0100

    Finished lesson 0

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,4 +1,5 @@
 #include "tgaimage.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -38,12 +39,26 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 
 int main(int argc, char** argv)
 {
-        TGAImage image(256, 256, TGAImage::RGB);
+    Model *model = new Model(argv[1]);
 
-        line(200, 256, 56, 0, image, red);
+    int w = 1024, h = 1024;
+    TGAImage image(w, h, TGAImage::RGB);
 
-        image.flip_vertically();
-        image.write_tga_file("output.tga");
+    for (int i=0; i<model->nfaces(); i++) { 
+        std::vector<int> face = model->face(i); 
+        for (int j=0; j<3; j++) { 
+            Vec3f v0 = model->vert(face[j]); 
+            Vec3f v1 = model->vert(face[(j+1)%3]); 
+            int x0 = (v0.x+1.)*w/2.; 
+            int y0 = (v0.y+1.)*h/2.; 
+            int x1 = (v1.x+1.)*w/2.; 
+            int y1 = (v1.y+1.)*h/2.; 
+            line(x0, y0, x1, y1, image, white); 
+        } 
+    }
 
-        return 0;
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+
+    return 0;
 }
